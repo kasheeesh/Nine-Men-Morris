3868bde2c42a32c28fb5f9143138f42bd6f2d108
LAKSHMI SAILAJA BURRA <141118858+blsailaja@users.noreply.github.com>
2024-12-23 09:26:15 +0530

    Add files via upload

---
+++ src/constants/soundPaths.ts
@@ -0,0 +1,7 @@
+export const SOUND_PATHS = {
+    shoot: '/sounds/shoot.mp3',
+    explosion: '/sounds/explosion.mp3',
+    powerup: '/sounds/powerup.mp3',
+    damage: '/sounds/damage.mp3',
+    gameOver: '/sounds/gameover.mp3'
+  } as const;
