297d8ad5fb1f1e4ea6b660e2467411a898e323f5
kasheesh <kashishbhurani@gmail.com>
2024-12-27 11:53:35 +0530

    Added the keyboard feature, chamged css

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,6 @@ import Lexi from './components/LexiQuest'
 import { Game } from './components/Game';
 import Games from './components/Games';
 
-// function App() {
-
-//   return (
-//     <>
-//       {/* <NineMensMorris/> */}
-//       {/* <Lexi/> */}
-//        <Game />
-//     </>
-//   )
-// }
-
-// export default App
 function App() {
   return (
     <Router>
